1594779d19a72c9ddfd6d76c800ceda5d5e5e160
linhtvhvmo <linhtvh@vmodev.com>
2023-02-16 17:34:49 +0700

    ignore check

--- web/index.js
+++ web/index.js
@@ -1,17 +1,16 @@
-// @ts-check
-import { join } from "path";
-import { readFileSync } from "fs";
-import express from "express";
-import serveStatic from "serve-static";
+import express from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import serveStatic from 'serve-static';
 
-import shopify from "./shopify.js";
-import productCreator from "./product-creator.js";
-import GDPRWebhookHandlers from "./gdpr.js";
+import GDPRWebhookHandlers from './gdpr.js';
+import productCreator from './product-creator.js';
+import shopify from './shopify.js';
 
 const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
 
 const STATIC_PATH =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? `${process.cwd()}/frontend/dist`
     : `${process.cwd()}/frontend/`;
 
@@ -22,26 +21,26 @@ app.get(shopify.config.auth.path, shopify.auth.begin());
 app.get(
   shopify.config.auth.callbackPath,
   shopify.auth.callback(),
-  shopify.redirectToShopifyOrAppRoot()
+  shopify.redirectToShopifyOrAppRoot(),
 );
 app.post(
   shopify.config.webhooks.path,
-  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
+  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers }),
 );
 
 // All endpoints after this point will require an active session
-app.use("/api/*", shopify.validateAuthenticatedSession());
+app.use('/api/*', shopify.validateAuthenticatedSession());
 
 app.use(express.json());
 
-app.get("/api/products/count", async (_req, res) => {
+app.get('/api/products/count', async (_req, res) => {
   const countData = await shopify.api.rest.Product.count({
     session: res.locals.shopify.session,
   });
   res.status(200).send(countData);
 });
 
-app.get("/api/products/create", async (_req, res) => {
+app.get('/api/products/create', async (_req, res) => {
   let status = 200;
   let error = null;
 
@@ -57,11 +56,11 @@ app.get("/api/products/create", async (_req, res) => {
 
 app.use(serveStatic(STATIC_PATH, { index: false }));
 
-app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
+app.use('/*', shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
   return res
     .status(200)
-    .set("Content-Type", "text/html")
-    .send(readFileSync(join(STATIC_PATH, "index.html")));
+    .set('Content-Type', 'text/html')
+    .send(readFileSync(join(STATIC_PATH, 'index.html')));
 });
 
 app.listen(PORT);
