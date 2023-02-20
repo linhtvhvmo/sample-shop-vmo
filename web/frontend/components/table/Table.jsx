f8665fbe4ad17b8ba9d44f10025b6cc89ff8ac58
linhtvhvmo <linhtvh@vmodev.com>
2023-02-20 09:33:03 +0700

    add index table

--- web/frontend/components/table/Table.jsx
+++ web/frontend/components/table/Table.jsx
@@ -1,56 +1,76 @@
-import { DataTable, Spinner, TextContainer } from '@shopify/polaris';
-import { useMemo } from 'react';
+import {
+  IndexTable,
+  Spinner,
+  TextContainer,
+  useIndexResourceState,
+} from '@shopify/polaris';
 
 export function CustomTable(props) {
   const { data, headings, ignoreFields, loading } = props;
 
-  const result = useMemo(() => {
-    if (loading)
-      return (
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Spinner size='large' />
-        </div>
-      );
-
-    if (!data || !data.length)
-      return <TextContainer>Error: data is undefined</TextContainer>;
-
-    if (!headings || !headings.length)
-      return <TextContainer> Error: no headings</TextContainer>;
-
-    const headers =
-      ignoreFields && ignoreFields.length
-        ? Object.keys(data[0]).filter(
-            (item) => item !== 'id' && !ignoreFields.includes(item),
-          )
-        : Object.keys(data[0]).filter((item) => item !== 'id');
-
-    const contentTypes = headers.map((item) => 'text');
-
-    const customData = data.map((item) => {
-      return headers.map((head) => {
-        return String(item[head]);
-      });
-    });
-
-    console.log(customData);
-    console.log(contentTypes);
-
+  if (loading)
     return (
-      <DataTable
-        stickyHeader
-        columnContentTypes={contentTypes}
-        headings={headings}
-        rows={customData}
-      />
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Spinner size='large' />
+      </div>
     );
-  }, [data, headings]);
-  return result;
+
+  if (!data || !data.length)
+    return <TextContainer>Error: data is undefined</TextContainer>;
+
+  if (!headings || !headings.length)
+    return <TextContainer> Error: no headings</TextContainer>;
+
+  const { selectedResources, allResourcesSelected, handleSelectionChange } =
+    useIndexResourceState(data);
+
+  const resourceName = {
+    singular: 'one_data',
+    plural: 'data',
+  };
+
+  const headers =
+    ignoreFields && ignoreFields.length
+      ? Object.keys(data[0]).filter(
+          (item) => item !== 'id' && !ignoreFields.includes(item),
+        )
+      : Object.keys(data[0]).filter((item) => item !== 'id');
+
+  const rowMarkup = data.map((item, index) => (
+    <IndexTable.Row
+      id={item.id}
+      key={item.id}
+      selected={selectedResources.includes(item.id)}
+      position={index}
+    >
+      {headers.map((head) => (
+        <IndexTable.Cell>{item[head]}</IndexTable.Cell>
+      ))}
+    </IndexTable.Row>
+  ));
+
+  const convertHeading = headings.map((item) => {
+    return { title: item };
+  });
+
+  return (
+    <IndexTable
+      resourceName={resourceName}
+      itemCount={data.length}
+      selectedItemsCount={
+        allResourcesSelected ? 'All' : selectedResources.length
+      }
+      onSelectionChange={handleSelectionChange}
+      headings={convertHeading}
+    >
+      {rowMarkup}
+    </IndexTable>
+  );
 }
